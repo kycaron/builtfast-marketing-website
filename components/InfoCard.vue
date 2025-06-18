ad2d0a2dc72491df91bb1f744794b51d5edbaa13
kycaron <kileycaron4@gmail.com>
2025-06-18 14:40:47 -0700

    Add `InfoCard` section of the homepage

---
+++ components/InfoCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import AppLayout from "@/layout/AppLayout.vue";
+
+</script>
+
+<template>
+<AppLayout>
+  <template #default>
+    <div class="bg-[#050f19] text-white rounded-lg h-72 lg:h-96 flex flex-col justify-center items-center space-y-6">
+    <slot name="title"/>
+    <slot name="cta"/>
+    </div>
+  </template>
+</AppLayout>
+</template>
+
+<style scoped>
+
+</style>
